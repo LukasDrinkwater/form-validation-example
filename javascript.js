1eb2ceeb2831c7f94967e2b1e29413d98cd1da4e
Lukas <lukasdrinkwater97@gmail.com>
2023-05-16 22:19:28 +0100

    started work on validation example

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const errorElement = document.getElementById("error");
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postcode = document.getElementById("postcode");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+form.addEventListener("submit", () => {});
